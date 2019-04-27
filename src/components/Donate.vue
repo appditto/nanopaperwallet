1b4a2ad08e6131593a6c900313103e3699465d74
yekta <ngyekta@gmail.com>
2019-04-27 19:48:29 +0300

    Temporary donation URI added

--- src/components/Donate.vue
+++ src/components/Donate.vue
@@ -12,7 +12,7 @@
             <h3 class="text-light w700 text-center text-lg-left mt-5 mt-lg-0">You like what we're doing?</h3>
             <h5 class="text-light text-center text-lg-left">Buy us a cup of coffee with NANO :)</h5>
             <div class="row d-flex justify-content-center justify-content-lg-start">
-              <button class="btn btn-lg btn-light btn-light-shadow text-primary w700 px-5 my-3 mx-3">Donate</button>
+              <a class="btn btn-lg btn-light btn-light-shadow text-primary w700 px-5 my-3 mx-3" href="nano:nano_1yekta1okrzh4hy3pjdmdghpp3ru557xxwfjt3xa4os951ji97h9tqthawoq?amount=10000000000000000000000000000">Donate</a>
             </div>
           </div>
         </div>
@@ -25,6 +25,6 @@
 import Vue from "vue";
 
 export default Vue.extend({
-  name: "Donate"
+  name: "Donate",
 });
 </script>
