5170d4d4d9aa88c91b21497ae3bc82d4c1185601
yekta <ngyekta@gmail.com>
2019-04-27 20:39:19 +0300

    Various changes

--- src/components/CreateGenerateBanner.vue
+++ src/components/CreateGenerateBanner.vue
@@ -1,34 +1,40 @@
 <template>
   <div class="section mt-4">
-      <div class="container-flex bg-footer py-5">
+    <div class="container-flex bg-footer py-5">
+      <div class="container-my mx-auto">
         <div class="row d-flex justify-content-center align-items-center">
-          <span class="h2 text-light">Design:</span>
-          <select class="ml-2 mr-4 dropdown-header" id="design">
-            <option value="a" selected="selected">A</option>
-            <option value="b">B</option>
-            <option value="c">C</option>
-            <option value="d">D</option>
-          </select>
-          <span class="h2 text-light">Add</span>
-          <select class="mx-2 dropdown-header" id="amount">
-            <option value="1" selected="selected">1</option>
-            <option value="5">5</option>
-            <option value="10">10</option>
-          </select>
-          <span class="h2 text-light">more paper wallets</span>
-          <button class="btn btn-lg btn-light btn-light-shadow text-primary w600 px-4 ml-4">Generate</button>
+        <span class="h3 text-light">Design:</span>
+        <select class="ml-2 mr-4 dropdown-header mb-2" id="design">
+          <option value="a" selected="selected">A</option>
+          <option value="b">B</option>
+          <option value="c">C</option>
+          <option value="d">D</option>
+        </select>
+        <span class="h3 text-light">Add</span>
+        <select class="mx-2 dropdown-header mb-2" id="amount">
+          <option value="1" selected="selected">1</option>
+          <option value="5">5</option>
+          <option value="10">10</option>
+        </select>
+        <span class="h3 text-light">more paper wallets</span>
+      </div>
+      <div class="row d-flex justify-content-center align-items-center mt-3">
+        <div class="col-8 col-md-7 col-lg-6 d-flex justify-content-center">
+          <button class="btn btn-lg btn-light btn-light-shadow text-primary w600 px-4 w-100">Generate</button>
         </div>
       </div>
-      <div class="row d-flex justify-content-center align-items-center mt-2">
-        <img class="icon-2" src="../assets/img/arrow.svg">
       </div>
     </div>
+    <div class="row d-flex justify-content-center align-items-center mt-2">
+      <img class="icon-2" src="../assets/img/arrow.svg">
+    </div>
+  </div>
 </template>
 
 <script>
 import Vue from "vue";
 
 export default Vue.extend({
-  name: "CreateGenerateBanner",
+  name: "CreateGenerateBanner"
 });
 </script>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,10 +2,10 @@
       <div class="container-my mx-auto">
         <div class="row d-flex justify-content-between align-items-center p-3">
           <a href="./">
-            <img src="../assets/img/logo.svg">
+            <img class="logo" src="../assets/img/logo.svg">
           </a>
           <button v-if = showButton
-            class="btn btn-lg btn-my btn-my-shadow text-light w600 px-4 px-lg-5"
+            class="btn btn-lg btn-my btn-my-shadow text-light w600 px-3 px-lg-5"
           >Download</button>
         </div>
       </div>
