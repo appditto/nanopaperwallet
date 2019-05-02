7e50f89926fb2c3411f3624dedad3f01a19bf6ba
yekta <ngyekta@gmail.com>
2019-05-02 04:06:37 +0300

    Footer tweaked

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="section bg-footer pt-4 pb-2">
     <div class="container-fluid">
-      <div class="row d-flex justify-content-center align-middle">
+      <div class="row d-flex justify-content-center align-middle px-2">
         <p class="text-light text-center w300">
-          © 2019. Made for <a target="_blank" href="https://reddit.com/u/rockmsockmjesus"><span class="w700">u/Rockmsockmjesus</span></a> by <a target="_blank" href="https://appditto.com"><span class="w700">Appditto</span></a>.
+          © 2019. Made for <a target="_blank" href="https://reddit.com/u/rockmsockmjesus"><span class="w700">u/Rockmsockmjesus</span></a> <br class="d-block d-md-none">by <a target="_blank" href="https://appditto.com"><span class="w700">Appditto</span></a>.
         </p>
       </div>
     </div>
