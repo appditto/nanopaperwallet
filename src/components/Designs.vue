af09976ca21f18a8212c6e9df161472beab834a9
yekta <ngyekta@gmail.com>
2019-05-07 01:41:54 +0300

    Changes on svgs

--- src/components/Designs.vue
+++ src/components/Designs.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="section py-5">
     <div class="container-my mx-auto py-4">
-      <div class="row d-flex justify-content-center justify-content-lg-start align-middle">
+      <div class="row d-flex justify-content-center align-middle">
         <div class="col-12">
           <h2 class="w700 text-center text-primary">Pick the One You Like</h2>
           <h5 class="w400 text-center text-dark col-12 col-md-10 col-lg-8 mx-auto">
             We have various colors that you can choose from.
             <br>So that everyone can find a color that suits them.
           </h5>
-          <div class="row d-flex justify-content-between align-middle">
+          <div class="row d-flex justify-content-center align-middle">
             <div class="col-12 col-md-6 mt-5 px-4">
               <img class="w-100" src="../assets/img/paperwalletA.svg">
             </div>
